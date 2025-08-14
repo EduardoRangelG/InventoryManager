c3c6ec122aa38e391f732d84ac3960df1126f555
EduardoRangelG <eduardo.rangel@encora.com>
2025-08-14 12:27:06 -0600

    feat: add controller layer

---
+++ backend/src/main/java/com/inventory/backend/controller/ProductController.java
@@ -0,0 +1,90 @@
+package com.inventory.backend.controller;
+
+import com.inventory.backend.model.Product;
+import com.inventory.backend.service.ProductService;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    // GET all products /products
+    @GetMapping
+    public ResponseEntity<Page<Product>> listProducts(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String category,
+            @RequestParam(required = false) Boolean inStock, Pageable pageable) {
+        Page<Product> productsPage = productService.listProducts(name, category, inStock, pageable);
+        return new ResponseEntity<>(productsPage, HttpStatus.OK);
+    }
+
+    // GET product by ID /products/{id}
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Integer id) {
+        return productService.getProductbyId(id).map(product -> new ResponseEntity<>(product, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    // POST product /products
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@Valid @RequestBody Product product) {
+        Product createdProduct = productService.createProduct(product);
+        return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
+    }
+
+    // PUT product by ID /product/{id}
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Integer id, @Valid @RequestBody Product productDetails) {
+        Product updatedProduct = productService.updateProduct(id, productDetails);
+        return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
+    }
+
+    // DELETE product by ID /product/{id}
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Product> deleteProduct(@PathVariable Integer id) {
+        productService.deleteProduct(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    // POST product outofstock by ID /product/{id}/outofstock
+    @PostMapping("/{id}/outofstock")
+    public ResponseEntity<Product> markProductOutOfStock(@PathVariable Integer id) {
+        Product updatedProduct = productService.markProductOutOfStock(id);
+        return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
+    }
+
+    // PUT product instock by ID /product/{id}/instock
+    @PutMapping("/{id}/instock")
+    public ResponseEntity<Product> markProductInStock(@PathVariable Integer id, @RequestBody Integer quantity) {
+        Product updatedProduct = productService.markProductInStock(id, quantity);
+        return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
+    }
+
+    // Handle bad requests
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
